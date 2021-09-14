8718c02b7298829a577519b8c02c6439ba7aae4b
dmytro.khl <dmytro.khludkov@vgs.io>
2021-09-14 15:19:13 +0300

    initial commit.

---
+++ TestScanSDK/TestScanSDK/TestScanSDK.h
@@ -0,0 +1,18 @@
+//
+//  TestScanSDK.h
+//  TestScanSDK
+//
+//  Created by Dima on 14.09.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestScanSDK.
+FOUNDATION_EXPORT double TestScanSDKVersionNumber;
+
+//! Project version string for TestScanSDK.
+FOUNDATION_EXPORT const unsigned char TestScanSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestScanSDK/PublicHeader.h>
+
+
---
+++ TestScanSDK/TestScanSDK/TestScanner.swift
@@ -0,0 +1,16 @@
+//
+//  TestScanner.swift
+//  TestScanSDK
+//
+//  Created by Dima on 14.09.2021.
+//
+
+import Foundation
+
+
+public class TestScanner {
+  
+  func open() {
+    print("TestScanner.open()")
+  }
+}
